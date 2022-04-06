7f6e894efb437a02124952f9e775f4e06a1ca10b
Pierre Jaffuer <p.jaffuer@free.fr>
2022-04-06 15:15:02 +0400

    wrong auto import

--- src/symbtable.py
+++ src/symbtable.py
@@ -1,7 +1,5 @@
 # TODO : use this in the parser AND in term.py to expend user defined terms during Beta reduction (left side only)
-from tokenize import Token
-
-
+from parser import Token
 class Symbtable:
     def __init__(self) -> None:
         self.tokens = []
